91fbad4b8204576f17b55857621a4246c79b38e0
Peter Tam <peter.tam@eyt3.com>
2024-01-19 09:44:26 +0800

    fixing for main file

--- src/main/java/com/peter/financeportfolio/FinancePortfolioApplication.java
+++ src/main/java/com/peter/financeportfolio/FinancePortfolioApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import javax.persistence.Entity;
 
 @SpringBootApplication
-@ComponentScan("com/peter/financeportfolio")
+@EntityScan("com.peter.financeportfolio.model.*")
+@EnableJpaRepositories("com.peter.financeportfolio.repository.*")
+@ComponentScan("com.peter.financeportfolio")
 public class FinancePortfolioApplication {
 
 	public static void main(String[] args) {
